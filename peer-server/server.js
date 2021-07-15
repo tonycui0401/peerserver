29a79b92babf6791e6679ec91bf8af42cf931a2e
tonycui0401 <tonycui0401@gmail.com>
2021-07-15 05:41:03 +0100

    update

--- peer-server/server.js
+++ peer-server/server.js
@@ -1,19 +1,51 @@
 require('dotenv').config()
-const express = require("express");
-const { ExpressPeerServer } = require("peer");
+// const express = require("express");
+// const { ExpressPeerServer } = require("peer");
+
+// const app = express();
+
+// app.get("/", (req, res, next) => res.send("Hello world!"));
+
+// const http = require("http");
+
+// const server = http.createServer(express);
+// const peerServer = ExpressPeerServer(server, {
+//   debug: true,
+//   path: "/",
+// });
+
+// app.use("/peerjs", peerServer);
+
+// server.listen(process.env.PORT || 9000);
+
+
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
 
 const app = express();
 
-app.get("/", (req, res, next) => res.send("Hello world!"));
+app.get('/', (req, res, next) => res.send('Hello world!'));
 
-const http = require("http");
+// =======
 
-const server = http.createServer(express);
+const server = app.listen(9000);
+
 const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: "/",
+  path: '/myapp'
 });
 
-app.use("/peerjs", peerServer);
+app.use('/peerjs', peerServer);
 
-server.listen(process.env.PORT || 9000);
+// == OR ==
+
+const http = require('http');
+
+const server = http.createServer(app);
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/myapp'
+});
+
+app.use('/peerjs', peerServer);
+
+server.listen(9000);
