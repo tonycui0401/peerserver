6a32a41124cc29fb0971bfd2effe558469500c17
tonycui0401 <tonycui0401@gmail.com>
2021-07-15 10:06:56 +0100

    update

--- peer-server/server.js
+++ peer-server/server.js
@@ -105,14 +105,56 @@ require('dotenv').config()
 // server.listen(port);
 // console.log('Listening on: ' + port);
 
-var fs = require('fs');
-var PeerServer = require('peer').PeerServer;
 
-var server = PeerServer({
-    port: 9000,
-    path: '/peerjs',
-    ssl: {
-        // key: fs.readFileSync('./../certificates/key.pem', 'utf8'),
-        // cert: fs.readFileSync('./../certificates/cert.pem', 'utf8')
-    }
+
+
+
+
+
+// var fs = require('fs');
+// var PeerServer = require('peer').PeerServer;
+
+// var server = PeerServer({
+//     port: 9000,
+//     path: '/peerjs',
+//     ssl: {
+//         // key: fs.readFileSync('./../certificates/key.pem', 'utf8'),
+//         // cert: fs.readFileSync('./../certificates/cert.pem', 'utf8')
+//     }
+// });
+
+
+
+
+const express = require("express");
+const http = require("http");
+const socketio = require("socket.io");
+const { ExpressPeerServer } = require("peer");
+// const mongoose = require("mongoose");
+// const config = require("config");
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server).sockets;
+
+//** Peer Server */
+const customGenerationFunction = () =>
+  (Math.random().toString(36) + "0000000000000000000").substr(2, 16);
+
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: "/",
+  generateClientId: customGenerationFunction,
 });
+
+app.use("/mypeer", peerServer);
+
+//** Config */
+// const db = config.get("mongoURI");
+
+//* Websocket *//
+io.on("connection", function (socket) {});
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`Server started on port ${port}`));
