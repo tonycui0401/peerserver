14437dc115b9b31bab2c3fdb621010e0159caf83
tonycui0401 <tonycui0401@gmail.com>
2021-07-16 10:49:26 +0100

    update

--- socket-server/app.js
+++ socket-server/app.js
@@ -77,17 +77,17 @@ io.on("connection", function(socket) {
 
 
 
-  socket.on("register", (username) => this.onRegister(socket, username));
-      socket.on("set-peer-id", (peerId) => this.onSetPeerId(socket, peerId));
-      socket.on("call", (username) => this.onCall(socket, username));
-      socket.on("reject-call", (username) =>
-        this.onRejectCall(socket, username)
-      );
-      socket.on("accept-call", (username) =>
-        this.onAcceptCall(socket, username)
-      );
-      console.log(`${Date(Date.now()).toLocaleString()}: new user connected`);
-      socket.on("disconnect", () => this.onDisconnect(socket));
+  // socket.on("register", (username) => this.onRegister(socket, username));
+  //     socket.on("set-peer-id", (peerId) => this.onSetPeerId(socket, peerId));
+  //     socket.on("call", (username) => this.onCall(socket, username));
+  //     socket.on("reject-call", (username) =>
+  //       this.onRejectCall(socket, username)
+  //     );
+  //     socket.on("accept-call", (username) =>
+  //       this.onAcceptCall(socket, username)
+  //     );
+  //     console.log(`${Date(Date.now()).toLocaleString()}: new user connected`);
+  //     socket.on("disconnect", () => this.onDisconnect(socket));
 
 
 
@@ -104,78 +104,78 @@ io.on("connection", function(socket) {
 
 
 
-onAcceptCall = (socket, username) => {
-  if (io.users[username])
-    io
-      .to(io.users[username].socketId)
-      .emit("accepted-call", io.users[socket.username]);
-};
+// onAcceptCall = (socket, username) => {
+//   if (io.users[username])
+//     io
+//       .to(io.users[username].socketId)
+//       .emit("accepted-call", io.users[socket.username]);
+// };
 
-onRejectCall = (socket, username) => {
-  if (io.users[username]) {
-    io
-      .to(io.users[username].socketId)
-      .emit("rejected-call", io.users[socket.username]);
-  }
-};
+// onRejectCall = (socket, username) => {
+//   if (io.users[username]) {
+//     io
+//       .to(io.users[username].socketId)
+//       .emit("rejected-call", io.users[socket.username]);
+//   }
+// };
 
-onCall = (socket, username) => {
-  if (io.users[username]) {
-    io.to(io.users[username].socketId)
-      .emit("call", io.users[socket.username]);
-  } else {
-    socket.emit("not-available", username);
-  }
-};
+// onCall = (socket, username) => {
+//   if (io.users[username]) {
+//     io.to(io.users[username].socketId)
+//       .emit("call", io.users[socket.username]);
+//   } else {
+//     socket.emit("not-available", username);
+//   }
+// };
 
-onRegister = (socket, username) => {
-  console.log("Registered", username);
-  socket.username = username;
-  io.users[username] = {
-    username,
-    peerId: "",
-    socketId: socket.id,
-  };
-  this.onUsersChange(socket);
-};
+// onRegister = (socket, username) => {
+//   console.log("Registered", username);
+//   socket.username = username;
+//   io.users[username] = {
+//     username,
+//     peerId: "",
+//     socketId: socket.id,
+//   };
+//   this.onUsersChange(socket);
+// };
 
-getUsers = () => {
-  const users = [];
-  Object.keys(io.users).forEach((key) => {
-    users.push(io.users[key]);
-  });
-  return users;
-};
+// getUsers = () => {
+//   const users = [];
+//   Object.keys(io.users).forEach((key) => {
+//     users.push(io.users[key]);
+//   });
+//   return users;
+// };
 
-onUsersChange = (socket) => {
-  io.emit("users-change", this.getUsers());
-};
+// onUsersChange = (socket) => {
+//   io.emit("users-change", this.getUsers());
+// };
 
-onSetPeerId = (socket, peerId) => {
-  console.log("Set Peer Id user:", socket.username, " peerId: ", peerId);
-  io.users[socket.username] = {
-    peerId,
-    socketId: socket.id,
-    username: socket.username,
-  };
-  this.onUsersChange();
-};
+// onSetPeerId = (socket, peerId) => {
+//   console.log("Set Peer Id user:", socket.username, " peerId: ", peerId);
+//   io.users[socket.username] = {
+//     peerId,
+//     socketId: socket.id,
+//     username: socket.username,
+//   };
+//   this.onUsersChange();
+// };
 
-onDisconnect = (socket) => {
-  delete io.users[socket.username];
-  console.log(
-    `${Date(Date.now()).toLocaleString()} ID:${
-      socket.username
-    } user disconnected`
-  );
-  this.onUsersChange();
-};
+// onDisconnect = (socket) => {
+//   delete io.users[socket.username];
+//   console.log(
+//     `${Date(Date.now()).toLocaleString()} ID:${
+//       socket.username
+//     } user disconnected`
+//   );
+//   this.onUsersChange();
+// };
 
-emit = (event, userId, data) => {
-  if (io.users[userId]) {
-    io.to(io.users[userId]).emit(event, data);
-  }
-};
+// emit = (event, userId, data) => {
+//   if (io.users[userId]) {
+//     io.to(io.users[userId]).emit(event, data);
+//   }
+// };
 
 
 
